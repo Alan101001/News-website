a3716bf95640be839de2e0afb433a4dc9349c619
Alain.K <147137153+Alan101001@users.noreply.github.com>
2024-06-09 20:27:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const container = document.querySelector(".container");
 const optionsContainer = document.querySelector(".options-container");
 const country = "in";
 const options = [
-  "general",
   "entertainment",
   "health",
   "science",
